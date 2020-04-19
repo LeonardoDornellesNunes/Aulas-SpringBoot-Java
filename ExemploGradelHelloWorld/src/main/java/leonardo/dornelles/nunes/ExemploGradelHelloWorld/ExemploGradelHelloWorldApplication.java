6ad30c2ed5f8b9c85cafdb4b80396de95daa661b
LeonardoDornellesNunes <leonardonunesdornelles@gmail.com>
2020-04-19 16:13:45 -0300

    Exemplo em Gradel de Hello World

---
+++ ExemploGradelHelloWorld/src/main/java/leonardo/dornelles/nunes/ExemploGradelHelloWorld/ExemploGradelHelloWorldApplication.java
@@ -0,0 +1,16 @@
+package leonardo.dornelles.nunes.ExemploGradelHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradelHelloWorldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExemploGradelHelloWorldApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ ExemploGradelHelloWorld/src/test/java/leonardo/dornelles/nunes/ExemploGradelHelloWorld/ExemploGradelHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.dornelles.nunes.ExemploGradelHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradelHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
