fee9a4fdb6eacb4a33b2e10831477e25c65f880d
LeonardoDornellesNunes <leonardonunesdornelles@gmail.com>
2020-04-19 15:59:06 -0300

    Exemplo em Gradel

---
+++ ExemploGradel/src/main/java/leonardo/dornelles/nunes/ExemploGradel/ExemploGradelApplication.java
@@ -0,0 +1,13 @@
+package leonardo.dornelles.nunes.ExemploGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradelApplication.class, args);
+	}
+
+}
---
+++ ExemploGradel/src/test/java/leonardo/dornelles/nunes/ExemploGradel/ExemploGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.dornelles.nunes.ExemploGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
